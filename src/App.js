1060f42d47d95930aef32768dcf7304910ffc374
Ali Alam <contact@aliserajalam.com>
2020-10-26 23:32:04 +1100

    Added page functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Navbar, Footer } from './components';
+import Home from './pages/Home/Home';
+import Services from './pages/Services/Services';
+import Products from './pages/Products/Products';
+import SignUp from './pages/SignUp/SignUp';
+
+import GlobalStyle from './globalStyles';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className='App'>
+      <Router>
+        <GlobalStyle />
+        <ScrollToTop />
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+          <Route path='/sign-up' component={SignUp} />
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import { Button } from '../../globalStyles';
+import {
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaTwitter,
+  FaLinkedin,
+} from 'react-icons/fa';
+import {
+  FooterContainer,
+  FooterSubscription,
+  FooterSubHeading,
+  FooterSubText,
+  Form,
+  FormInput,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterLinksItems,
+  FooterLinkTitle,
+  FooterLink,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  SocialIcon,
+  WebsiteRights,
+  SocialIcons,
+  SocialIconLink,
+} from './Footer.elements';
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterSubscription>
+        <FooterSubHeading>
+          Join our exclusive membership to receive the latest news and trends
+        </FooterSubHeading>
+        <FooterSubText>You can unsubscribe at any time</FooterSubText>
+        <Form>
+          <FormInput
+            name='email'
+            type='email'
+            placeholder='Your email address'
+          />
+          <Button fontBig>Subscribe</Button>
+        </Form>
+      </FooterSubscription>
+      <FooterLinksContainer>
+        <FooterLinksWrapper>
+          <FooterLinksItems>
+            <FooterLinkTitle>About Us</FooterLinkTitle>
+            <FooterLink to='/sign-up'>How it works</FooterLink>
+            <FooterLink to='/'>Testimonials</FooterLink>
+            <FooterLink to='/'>Careers</FooterLink>
+            <FooterLink to='/'>Investors</FooterLink>
+            <FooterLink to='/'>Terms of Service</FooterLink>
+          </FooterLinksItems>
+          <FooterLinksItems>
+            <FooterLinkTitle>Contact Us</FooterLinkTitle>
+            <FooterLink to='/sign-up'>How it works</FooterLink>
+            <FooterLink to='/'>Testimonials</FooterLink>
+            <FooterLink to='/'>Careers</FooterLink>
+            <FooterLink to='/'>Investors</FooterLink>
+            <FooterLink to='/'>Terms of Service</FooterLink>
+          </FooterLinksItems>
+        </FooterLinksWrapper>
+        <FooterLinksWrapper>
+          <FooterLinksItems>
+            <FooterLinkTitle>Videos</FooterLinkTitle>
+            <FooterLink to='/sign-up'>How it works</FooterLink>
+            <FooterLink to='/'>Testimonials</FooterLink>
+            <FooterLink to='/'>Careers</FooterLink>
+            <FooterLink to='/'>Investors</FooterLink>
+            <FooterLink to='/'>Terms of Service</FooterLink>
+          </FooterLinksItems>
+          <FooterLinksItems>
+            <FooterLinkTitle>Social Media</FooterLinkTitle>
+            <FooterLink to='/sign-up'>How it works</FooterLink>
+            <FooterLink to='/'>Testimonials</FooterLink>
+            <FooterLink to='/'>Careers</FooterLink>
+            <FooterLink to='/'>Investors</FooterLink>
+            <FooterLink to='/'>Terms of Service</FooterLink>
+          </FooterLinksItems>
+        </FooterLinksWrapper>
+      </FooterLinksContainer>
+      <SocialMedia>
+        <SocialMediaWrap>
+          <SocialLogo to='/'>
+            <SocialIcon />
+            ULTRA
+          </SocialLogo>
+          <WebsiteRights>ULTRA 2020</WebsiteRights>
+          <SocialIcons>
+            <SocialIconLink href='/' target='_blank' aria-label='Facebook'>
+              <FaFacebook />
+            </SocialIconLink>
+            <SocialIconLink href='/' target='_blank' aria-label='Instagram'>
+              <FaInstagram />
+            </SocialIconLink>
+            <SocialIconLink
+              href='/'
+              target='_blank'
+              aria-label='Youtube'
+              rel='noopener noreferrer'
+            >
+              <FaYoutube />
+            </SocialIconLink>
+            <SocialIconLink href='/' target='_blank' aria-label='Twitter'>
+              <FaTwitter />
+            </SocialIconLink>
+            <SocialIconLink href='/' target='_blank' aria-label='Linkedin'>
+              <FaLinkedin />
+            </SocialIconLink>
+          </SocialIcons>
+        </SocialMediaWrap>
+      </SocialMedia>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
